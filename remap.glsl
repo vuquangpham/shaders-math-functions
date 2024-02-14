b0e208e3e187af38d339da4c5a9adee7f82eada7
vuquangpham <vuquangpham2909@gmail.com>
2024-02-14 23:28:41 +0700

    feaT: remap

--- remap.glsl
+++ remap.glsl
@@ -1,3 +1,9 @@
+// inverse lerp
+// return in percentage [0.0, 1.0]
+float inverseLerp(float v, float minValue, float maxValue) {
+  return (v - minValue) / (maxValue - minValue);
+}
+
 float remap(float v, float inMin, float inMax, float outMin, float outMax) {
   float t = inverseLerp(v, inMin, inMax);
   return mix(outMin, outMax, t);
